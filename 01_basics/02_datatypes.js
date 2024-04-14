130cfcab2fcf965bf87c99d2e55e060898dc2271
Divyanjali Sharma <157833347+143divya@users.noreply.github.com>
2024-04-14 10:22:23 +0000

    datatypes'

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" ;// treat all js code as newer version
+
+// alert( 3+3) // we are using NOdejs , not browser 
+
+console.log(3
++
+3) // code readability should be on high priority
+
+console.log("Divyanjali")
+
+let name = "Divyanjali" 
+let age = 19
+let IsloggedIn = false
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false 
+//null => standalone value
+// undefined =>
+//symbol => unique
+
+//object
+console.log(typeof "Divyanjali")
+console.log(typeof null);//object
+console.log(typeof undefined); //undefined
+
