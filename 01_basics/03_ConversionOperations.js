fcfb8ff88be6b6cc81bc36d9b7bfeff24755d6a9
Divyanjali Sharma <157833347+143divya@users.noreply.github.com>
2024-04-14 11:05:48 +0000

    Conversion Operations

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; / false =>0;
+
+let IsloggedIn = 1;
+let IsloggedIn = "";
+let IsloggedIn = "Divyanjali";
+
+let booleanIsloggedIn = Boolean(IsloggedIn)
+console.log(booleanIsloggedIn);
+
+//0 => false; / 1 => true;
+// "" => false;
+//"Divyanjali" =>true;
+
+let someNumber = 33
+let StringNumber =  String(someNumber)
+console.log( typeof StringNumber)
+
